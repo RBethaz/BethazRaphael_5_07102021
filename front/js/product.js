8e90c58f49e82fdc16f2709b287e4a6d416f0307
BethazRaphael <raphael.bethaz@hotmail.com>
2021-10-20 15:10:37 +0200

    JS_product qql modif

--- front/js/product.js
+++ front/js/product.js
@@ -1,154 +1,149 @@
-// Récupération des paramètres depuis l'URL
+// Récupération des paramètres depuis l'URL                                                     // ---------- ↓↓↓↓↓ Commentaires persos ↓↓↓↓↓ ----------
 
-var str = window.location.href;
-var url = new URL(str);
-var idProduct = url.searchParams.get("id");
-
+let str = window.location.href;                                                                 // ---------- window.location.href indique l'URL actuelle ----------       
+let url = new URL(str);
+let idProduct = url.searchParams.get("id");                                                     // ---------- Je récupère mon id via URLSearchParams pour afficher mon article ----------
 console.log(idProduct);
 
-const colorPicked = document.querySelector("#colors");
-const quantityPicked = document.querySelector("#quantity");
 
-
-// Récupération des articles depuis l'API
+// Interroger l’API pour récupérer les détails du produit
 
 getArticle();
 
-function getArticle() {
-    fetch("http://localhost:3000/api/products/" + idProduct)
-    .then(function (res) {
+function getArticle() {                                                                     
+    fetch("http://localhost:3000/api/products/" + idProduct)                                    // ---------- J'envoie une requête HTTP ----------
+    .then(function (res) {                                                                      // ---------- Je récupère le résultat de la requête ----------
         return res.json();
     })
     .catch((error) => {
         console.log("Accès à l'API impossible... Déso frérot !!!");
     })
-}
+
 
 // Récupération des données de l'API dans le DOM --> Affichage des données des articles
 
-then(function (articlesResult) {
-    const article = articlesResult;
-    console.table(article);
+    .then(articlesResult => {
+        const article = articlesResult;                                                         // ---------- Fonction anonyme --> = function(articlesResult) ... ----------
+        console.table(article);                                                                 // ---------- Je récupère mes articles sous forme de tableau ----------
 
 
-    // Création de "img" (image)
+        // Création + affichage de l'image
 
-    let productImg = document.createElement("img");                                         // ---------- Je crée mon "img" (image) ----------
-    document.querySelector(".item__img").appendChild(productImg);
-    productImg.src = article.imageUrl;                                                      // ---------- J'affiche l'image de l'article ----------
-    productImg.alt = article.altTxt;                                                        // ---------- Je rapatrie le texte alternatif de l'image de l'article ----------
-
-
-    // Modification de "h1"
-
-    let productName = document.getElementById('title');
-    productName.innerHTML = article.name;                                                   // ---------- J'affiche le nom de l'article ----------                                             
+        let productImg = document.createElement("img");                                         // ---------- Je crée mon "img" (image) ----------
+        document.querySelector(".item__img").appendChild(productImg);
+        productImg.src = article.imageUrl;                                                      // ---------- J'affiche l'image de l'article ----------
+        productImg.alt = article.altTxt;                                                        // ---------- Je rapatrie le texte alternatif de l'image de l'article ----------
 
 
-    // Affichage du prix
+        // Affichage du nom
 
-    let productPrice = document.getElementById('price');
-    productPrice.innerHTML = article.price;                                                 // ---------- J'affiche le prix de l'article ----------
-
-
-    // Affichage de la description
-
-    let productDescription = document.getElementById('description');
-    productDescription.innerHTML = article.description;                                     // ---------- J'affiche la description de l'article ----------
+        let productName = document.getElementById('title');
+        productName.innerHTML = article.name;                                                   // ---------- J'affiche le nom de l'article ----------                                             
 
 
-    // Choix des couleurs
+        // Affichage du prix
 
-    for (let colors of article.colors){
-        console.table(colors);
-        let productColors = document.createElement("option");                               // ---------- J'affiche mon selecteur de couleur ----------
-        document.querySelector("#colors").appendChild(productColors);
-        productColors.value = colors;
-        productColors.innerHTML = colors;
-    }
+        let productPrice = document.getElementById('price');
+        productPrice.innerHTML = article.price;                                                 // ---------- J'affiche le prix de l'article ----------
 
 
-// Gestion du panier
+        // Affichage de la description
 
-const sendToCartBtn = document.querySelector("#addToCart");
-
-
-// Conditions couleur + quantité entre 1 et 100
-
-sendToCartBtn.addEventListener("click", (event)=>{
-    if (quantityPicked.value > 0 && quantityPicked.value <=100 && colorPicked.value != 0){
+        let productDescription = document.getElementById('description');
+        productDescription.innerHTML = article.description;                                     // ---------- J'affiche la description de l'article ----------
 
 
-// Récupération du choix de la couleur
+        // Création du selecteur de couleur + Affichage de la couleur choisie
 
-let colorChoice = colorPicked.value;
+        const selectedColor = document.querySelector("#colors");
+        const selectedQuantity = document.querySelector("#quantity");
 
+        
+        article.colors.forEach(color => {
+            let productColors = document.createElement("option");                               // ---------- Je crée mon selecteur de couleur ----------
+            document.querySelector("#colors").appendChild(productColors);
+            productColors.value = color;
+            productColors.innerHTML = color;
+        })
 
-// Récupération du choix de la quantité
+        // Gestion du panier
 
-let quantityChoice = quantityPicked.value;
-
-
-// Récupération des options de l'article à ajouter au panier
-
-let optionsProduit = {
-    articleID: idProduct,
-    articleColor: colorChoice,
-    articleQuantity: Number(quantityChoice)
-};
-
-   // Initialisation du local storage
-
-   let localStorageArticle = JSON.parse(localStorage.getItem("produit"));
+        const sendToCartBtn = document.querySelector("#addToCart");                             // ---------- Je sélectionne mon bouton "Ajouter au panier" ----------
 
 
-   // Pop-up de confirmation
+        // Conditions couleur + quantité entre 1 et 100
 
-   const popupConfirmation =() =>{
-       if(window.confirm(`Votre commande de ${quantityChoice} ${article.name} ${colorChoice} à bien été ajoutée au panier
-Pour consulter votre panier, cliquez sur OK`)){
-           window.location.href ="cart.html";
-       }
-   }
+        sendToCartBtn.addEventListener("click", (event) => {                                    // ---------- Je réagis au click sur le bouton ----------
+            if (selectedQuantity.value > 0 && selectedQuantity.value <=100 && selectedColor.value != 0){
 
-
-   //Importation dans le local storage
+        let colorChoice = selectedColor.value;
+        let quantityChoice = selectedQuantity.value;
 
 
-       //Si le panier comporte déjà au moins 1 article
+        // Récupération des options de l'article + ajout au panier
 
-   if (localStorageArticle) {
-   const resultFind = localStorageArticle.find(
-       (el) => el.articleID === idProduct && el.articleColor === colorChoice);
+        let articleOptions = {                                                                  // ---------- Je crée mon Array "options de l'article" ----------
+            articleID: idProduct,
+            articleColor: colorChoice,
+            articleQuantity: quantityChoice,
+            articleName: article.name,
+            articlePrice: article.price,
+            articleDesc: article.description,
+            articleImg: article.imageUrl,
+            articleImgAlt: article.altTxt
+        };
 
-       //Si le produit commandé est déjà dans le panier
 
-       if (resultFind) {
-           let newQuantite =
-           parseInt(optionsProduit.articleQuantity) + parseInt(resultFind.articleQuantity);
-           resultFind.articleQuantity = newQuantite;
-           localStorage.setItem("produit", JSON.stringify(localStorageArticle));
-           console.table(localStorageArticle);
-           popupConfirmation();
+        // Initialisation du local storage
 
-       //Si le produit commandé n'est pas dans le panier
+        let localStorageArticle = JSON.parse(localStorage.getItem("produit"));
 
-       } else {
-           localStorageArticle.push(optionsProduit);
-           localStorage.setItem("produit", JSON.stringify(localStorageArticle));
-           console.table(localStorageArticle);
-           popupConfirmation();
-       }
 
-   //Si le panier est vide
+        // Pop-up de confirmation
 
-   } else {
-       localStorageArticle =[];
-       localStorageArticle.push(optionsProduit);
-       localStorage.setItem("produit", JSON.stringify(localStorageArticle));
-       console.table(localStorageArticle);
-       popupConfirmation();
-   }}
-   });
-});
+        const popupConfirmation = () => {                                                       // ---------- Je crée ma pop-up de confirmation d'ajout au panier ----------
+            if(window.confirm(`Votre commande de ${quantityChoice} ${article.name} ${colorChoice} à bien été ajoutée au panier
+    Pour consulter votre panier, cliquez sur OK`)){
+                window.location.href ="cart.html";
+            }
+        }
 
+
+        //Importation dans le local storage
+
+
+            // --> Le panier comporte déjà au moins 1 article
+
+            if (localStorageArticle) {
+            const resultFind = localStorageArticle.find(
+                (article) => article.articleID === idProduct && article.articleColor === colorChoice
+                );
+            
+
+            // --> Le produit ajouté est déjà dans le panier
+
+                if (resultFind) {
+                    resultFind.articleQuantity = parseInt(articleOptions.articleQuantity) + parseInt(resultFind.articleQuantity);
+
+
+            // --> Le produit commandé n'est pas dans le panier
+
+                } else {
+                    localStorageArticle.push(articleOptions);
+                }
+                console.table(localStorageArticle);
+                localStorage.setItem("produit", JSON.stringify(localStorageArticle));
+                popupConfirmation();
+
+            // --> Le panier est vide
+
+            } else {
+                localStorageArticle =[];
+                localStorageArticle.push(articleOptions);
+                localStorage.setItem("produit", JSON.stringify(localStorageArticle));
+                console.table(localStorageArticle);
+                popupConfirmation();
+            }}
+        });
+    });
+}
