7b676cb48116499b4493956b1a66cf51e90b3e8f
BethazRaphael <raphael.bethaz@hotmail.com>
2021-10-20 15:15:47 +0200

    JS_cart recuperation array depuis local storage

---
+++ front/js/cart.js
@@ -0,0 +1,5 @@
+// Récupération depuis localStorage
+
+let produitLocalStorage = JSON.parse(localStorage.getItem("produit"));
+console.table(produitLocalStorage);
+const positionEmptyCart = document.querySelector("#cart__items");
