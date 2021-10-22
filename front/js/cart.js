0664cd8ba7057bb340d3c0b33c92daa27feb499d
BethazRaphael <raphael.bethaz@hotmail.com>
2021-10-22 08:51:18 +0200

    JS_cart si panier vide

--- front/js/cart.js
+++ front/js/cart.js
@@ -3,3 +3,9 @@
 let produitLocalStorage = JSON.parse(localStorage.getItem("produit"));
 console.table(produitLocalStorage);
 const positionEmptyCart = document.querySelector("#cart__items");
+
+// --> Le panier est vide
+if (produitLocalStorage === null || produitLocalStorage == 0) {
+    const emptyCart = `<p>Votre panier est vide</p>`;
+    positionEmptyCart.innerHTML = emptyCart;
+}
